d39e6ea9320c691c1794811ad41562663ae6f385
unknown <asep.cahyudin92@gmail.com>
2022-11-25 23:09:25 +0700

    pre splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:saveshamo/pages/splash_page.dart';
+import 'theme.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Saya Sukses',
-          style: GoogleFonts.poppins(
-            fontSize: 50,
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashPage());
   }
 }
