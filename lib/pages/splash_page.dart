6f9c0f7f3070692fa41f7f2ca464e4b49adedc85
unknown <asep.cahyudin92@gmail.com>
2022-11-25 23:50:54 +0700

    splash image

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:saveshamo/theme.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor1,
       body: Center(
-        child: Text('Splash Page'),
+        child: Container(
+          width: 130,
+          height: 150,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/Splash_Image.png',
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
