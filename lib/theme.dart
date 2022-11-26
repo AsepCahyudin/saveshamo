9480732d229ac6921ac5adc6bff11e036bc6c501
unknown <asep.cahyudin92@gmail.com>
2022-11-26 07:06:01 +0700

    login page email

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 30.0;
+
 Color primaryColor = Color(0xff6C5ECF);
 Color secondaryColor = Color(0xff38ABBE);
 Color alertColor = Color(0xffED6363);
@@ -10,12 +12,17 @@ Color backgroundColor2 = Color(0xff2B2937);
 Color backgroundColor3 = Color(0xff242231);
 Color primaryTextColor = Color(0xffF1F0F2);
 Color secondaryTextColor = Color(0xff999999);
+Color subtitleColor = Color(0xff504F5E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
 );
 
 TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
   color: secondaryTextColor,
 );
 
