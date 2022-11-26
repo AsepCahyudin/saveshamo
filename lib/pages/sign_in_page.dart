d2ade5dc622fd82f15636549c92b1dd572ad7f8b
unknown <asep.cahyudin92@gmail.com>
2022-11-26 07:31:25 +0700

    login page password

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -69,6 +69,47 @@ class SignInPage extends StatelessWidget {
       );
     }
 
+    Widget passwordInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text('Password',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium)),
+          SizedBox(
+            height: 12,
+          ),
+          Container(
+            height: 50,
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(12)),
+            child: Center(
+                child: Row(
+              children: [
+                Image.asset(
+                  'assets/Password_Icon.png',
+                  width: 17,
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                    child: TextFormField(
+                  style: primaryTextStyle,
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Your Password',
+                    hintStyle: subtitleTextStyle,
+                  ),
+                ))
+              ],
+            )),
+          )
+        ]),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor1,
       body: Container(
@@ -80,6 +121,7 @@ class SignInPage extends StatelessWidget {
           children: [
             header(),
             emailInput(),
+            passwordInput(),
           ],
         ),
       ),
