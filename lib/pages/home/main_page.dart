c439a87d80b26ec8e706eb2a3bb0337be02f31e6
unknown <asep.cahyudin92@gmail.com>
2022-11-28 06:33:19 +0700

    set up style navigation

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -7,9 +7,56 @@ class MainPage extends StatelessWidget {
     Widget cartButton() {
       return FloatingActionButton(
         onPressed: () {},
+        backgroundColor: secondaryColor,
         child: Image.asset(
           'assets/Cart_Icon.png',
           width: 20,
+        ),
+      );
+    }
+
+    Widget customBottomnNav() {
+      return ClipRRect(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+              backgroundColor: backgroundColor4,
+              type: BottomNavigationBarType.fixed,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/Home.png',
+                    width: 21,
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/Chat_Icon.png',
+                    width: 21,
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/Favorite_Icon.png',
+                    width: 20,
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/Profile.png',
+                    width: 18,
+                  ),
+                  label: '',
+                ),
+              ]),
         ),
       );
     }
@@ -18,6 +65,7 @@ class MainPage extends StatelessWidget {
       backgroundColor: backgroundColor1,
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customBottomnNav(),
       body: Center(child: Text('Main Page')),
     );
   }
