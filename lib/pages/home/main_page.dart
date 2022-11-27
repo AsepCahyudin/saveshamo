8385b511a056a811ae421423db4e21e337011aa9
unknown <asep.cahyudin92@gmail.com>
2022-11-28 05:42:24 +0700

    set up button

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:saveshamo/theme.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget cartButton() {
+      return FloatingActionButton(
+        onPressed: () {},
+        child: Image.asset(
+          'assets/Cart_Icon.png',
+          width: 20,
+        ),
+      );
+    }
+
     return Scaffold(
+      backgroundColor: backgroundColor1,
+      floatingActionButton: cartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: Center(child: Text('Main Page')),
     );
   }
