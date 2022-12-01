958ff450f3c293535ae8d8f640f1f58c7a1ed066
unknown <asep.cahyudin92@gmail.com>
2022-12-01 18:19:52 +0700

    image avatar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -14,8 +14,33 @@ class HomePage extends StatelessWidget {
         child: Row(
           children: [
             Column(
-              children: [Text('Hallo, Save')],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hallo, Save',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                Text(
+                  '@savefaaizuun',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                )
+              ],
             ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/Image_Profile.png'),
+                ),
+              ),
+            )
           ],
         ),
       );
