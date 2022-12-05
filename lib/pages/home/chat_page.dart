4dfa4d730a13d5d3accade7d93b67d0bf8ffc077
unknown <asep.cahyudin92@gmail.com>
2022-12-05 23:09:12 +0700

    appbar chat

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -4,11 +4,24 @@ import 'package:saveshamo/theme.dart';
 class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Chat Page',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Message Support',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
