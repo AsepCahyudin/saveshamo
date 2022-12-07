a7bbb35a99452e2c62c467acd80ce43d3373c911
unknown <asep.cahyudin92@gmail.com>
2022-12-07 21:44:16 +0700

    add file detail chat

---
+++ lib/pages/detail_chat.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:saveshamo/theme.dart';
+
+class DetailChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
