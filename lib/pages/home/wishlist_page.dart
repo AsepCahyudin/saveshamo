6b118c6ae9e4022ae9dcc3c291d1d422daa0941c
unknown <asep.cahyudin92@gmail.com>
2022-12-14 06:01:34 +0700

    whislist header

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -4,11 +4,22 @@ import 'package:saveshamo/theme.dart';
 class WishlistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Wishlist Page',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Favorite Shoes',
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
