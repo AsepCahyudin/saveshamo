074e24a299bcc8d30e0b27c22cd6824a9c3b9cff
unknown <asep.cahyudin92@gmail.com>
2022-12-13 17:44:12 +0700

    prepare cha buble product preview

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -9,36 +9,59 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget productPreview() {
+      return Container(
+        width: 230,
+        height: 155,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(isSender ? 12 : 0),
+            topRight: Radius.circular(isSender ? 0 : 12),
+            bottomLeft: Radius.circular(12),
+            bottomRight: Radius.circular(12),
+          ),
+          color: isSender ? backgroundColor5 : backgroundColor4,
+        ),
+      );
+    }
+
     return Container(
       width: double.infinity,
       margin: EdgeInsets.only(top: 30),
-      child: Row(
-        mainAxisAlignment:
-            isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+      child: Column(
+        crossAxisAlignment:
+            isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
-          Flexible(
-            child: Container(
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.6,
-              ),
-              padding: EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 12,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(isSender ? 12 : 0),
-                  topRight: Radius.circular(isSender ? 0 : 12),
-                  bottomLeft: Radius.circular(12),
-                  bottomRight: Radius.circular(12),
+          productPreview(),
+          Row(
+            mainAxisAlignment:
+                isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Container(
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.6,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(isSender ? 12 : 0),
+                      topRight: Radius.circular(isSender ? 0 : 12),
+                      bottomLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    ),
+                    color: isSender ? backgroundColor5 : backgroundColor4,
+                  ),
+                  child: Text(
+                    text,
+                    style: primaryTextStyle,
+                  ),
                 ),
-                color: isSender ? backgroundColor5 : backgroundColor4,
               ),
-              child: Text(
-                text,
-                style: primaryTextStyle,
-              ),
-            ),
+            ],
           ),
         ],
       ),
