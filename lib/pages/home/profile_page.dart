2c089277a0687bf86fb6d1724eea5c0841fa2c6b
unknown <asep.cahyudin92@gmail.com>
2022-12-16 21:13:54 +0700

    header profile

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -5,7 +5,55 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget header() {
-      return AppBar();
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(
+              defaultMargin,
+            ),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/Image_Profile.png',
+                    width: 64,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hallo, Save',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      Text(
+                        '@savefaaizuun',
+                        style: subtitleTextStyle.copyWith(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/Exit_Button.png',
+                  width: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
     }
 
     return Column(
