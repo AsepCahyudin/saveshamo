f4a3361a2a3cf06bf7bdab898b737ed76ec9f8cf
unknown <asep.cahyudin92@gmail.com>
2022-12-17 08:49:22 +0700

    header edit profile

---
+++ lib/pages/edit_profile_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:saveshamo/theme.dart';
+
+class EditProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    PreferredSizeWidget header() {
+      return AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.close),
+        ),
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Edit Profile',
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.check,
+              color: primaryColor,
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget content() {
+      return Container();
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+    );
+  }
+}
