1c3304ea65a2c5a9d280cd4ef1571bd0a9862ce1
unknown <asep.cahyudin92@gmail.com>
2022-12-19 21:26:24 +0700

    detail product header

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -4,9 +4,43 @@ import 'package:saveshamo/theme.dart';
 class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: 20,
+              left: defaultMargin,
+              right: defaultMargin,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Icon(
+                    Icons.chevron_left,
+                  ),
+                ),
+                Icon(
+                  Icons.shopping_bag,
+                  color: backgroundColor1,
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text('Product Page'),
+      backgroundColor: backgroundColor6,
+      body: ListView(
+        children: [
+          header(),
+        ],
       ),
     );
   }
