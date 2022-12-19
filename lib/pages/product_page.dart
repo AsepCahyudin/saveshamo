dfeec64c31368cf2c2db15bdb5b675a1b40b2190
unknown <asep.cahyudin92@gmail.com>
2022-12-19 18:05:05 +0700

    product page prepare

---
+++ lib/pages/product_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:saveshamo/theme.dart';
+
+class ProductPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Product Page'),
+      ),
+    );
+  }
+}
