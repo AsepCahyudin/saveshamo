e4a347805536912c5cc0f38f507a10e85e376318
unknown <asep.cahyudin92@gmail.com>
2022-12-28 15:21:14 +0700

    empty cart

--- lib/pages/cart_pages.dart
+++ lib/pages/cart_pages.dart
@@ -5,12 +5,34 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     PreferredSizeWidget header() {
-      return AppBar();
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Your Cart',
+        ),
+        elevation: 0,
+      );
+    }
+
+    Widget emptyCart() {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/Empty_Cart_Icon.png',
+              width: 80,
+            ),
+          ],
+        ),
+      );
     }
 
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
+      body: emptyCart(),
     );
   }
 }
