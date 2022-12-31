5fc0bc43590428e854bb02c673f07a4e7765d893
unknown <asep.cahyudin92@gmail.com>
2022-12-31 17:13:28 +0700

    checkout pgae header

--- lib/widgets/checkout_card.dart
+++ lib/widgets/checkout_card.dart
@@ -10,6 +10,10 @@ class CheckoutCard extends StatelessWidget {
         vertical: 20,
         horizontal: 12,
       ),
+      decoration: BoxDecoration(
+        color: backgroundColor4,
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: Row(
         children: [
           Container(
@@ -23,7 +27,40 @@ class CheckoutCard extends StatelessWidget {
                 ),
               ),
             ),
-          )
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Terrex Urban Low',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: semiBold,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  '\$154,76',
+                  style: priceTextStyle,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Text(
+            '2 Items',
+            style: secondaryTextStyle.copyWith(
+              fontSize: 12,
+            ),
+          ),
         ],
       ),
     );
