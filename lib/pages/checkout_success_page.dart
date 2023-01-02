7598754cd37ee94656020593a3f08e5526d716c6
unknown <asep.cahyudin92@gmail.com>
2023-01-03 06:37:14 +0700

    add checkout success page

---
+++ lib/pages/checkout_success_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:saveshamo/theme.dart';
+
+class CheckoutSuccessPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
