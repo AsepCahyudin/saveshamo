4341cf5891e5ca816bea32f719c6347bfe381ebe
unknown <asep.cahyudin92@gmail.com>
2023-01-05 23:10:26 +0700

    return user

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,19 +1,39 @@
+import 'dart:convert';
+
 import 'package:saveshamo/models/user_model.dart';
 
 class AuthService{
 
   String baseUrl = 'https:shamo-backend.buildwithangga.id/api';
+  
+  get http => null;
 
   Future<UserModel> register({
     required String name, 
+    required String username,
+    required String email,
+    required String password,
   }) async {
-    var url ='test';
+    var url ='$baseUrl/register';
+    var headers = {'Content-Type': 'application/json'};
+    var body = jsonEncode({
+      'name' = name,
+      'username' = username,
+      'email' = email,
+      'password' = password,
+    });
 
     var response = await http.post(
       url,
+      headers: headers,
+      body: body
     );
 
     if(response.statusCode == 200) {
+      var data = jsonDecode(response.body)['data'];
+      UserModel user = UserModel.fromJson(data['user']);
+      user.token = 'Bearer ' + data['access_token'];
+
       return user;
     } else {
       throw Exception('gagal');
