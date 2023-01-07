80c651759dba9ecd97216b71a8f3cfe801419b29
unknown <asep.cahyudin92@gmail.com>
2023-01-07 11:16:33 +0700

    solved bug auth service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,32 +3,28 @@ import 'dart:convert';
 import 'package:saveshamo/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
-class AuthService{
-
+class AuthService {
   String baseUrl = 'https://shamo-backend.buildwithangga.id/api';
 
   Future<UserModel> register({
-    String name, 
+    String name,
     String username,
     String email,
     String password,
   }) async {
-    var url ='$baseUrl/register';
+    var url = '$baseUrl/register';
     var headers = {'Content-Type': 'application/json'};
     var body = jsonEncode({
-      'name' = name,
-      'username' = username,
-      'email' = email,
-      'password' = password,
+      'name': name,
+      'username': username,
+      'email': email,
+      'password': password,
     });
 
-    var response = await http.post(
-      url,
-      headers: headers,
-      body: body
-    );
+    var response =
+        await http.post(Uri.parse(url), headers: headers, body: body);
 
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       var data = jsonDecode(response.body)['data'];
       UserModel user = UserModel.fromJson(data['user']);
       user.token = 'Bearer ' + data['access_token'];
