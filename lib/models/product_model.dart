b5c670efc125838c607736d9854e4bdf5007fc26
unknown <asep.cahyudin92@gmail.com>
2023-01-13 06:26:40 +0700

    add product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,45 @@
+import 'package:saveshamo/models/category_model.dart';
+import 'package:saveshamo/models/gallery_model.dart';
+
+class ProductModel {
+  int id;
+  String name;
+  double price;
+  String description;
+  String tags;
+  CategoryModel category;
+  DateTime createdAt;
+  DateTime updatedAt;
+  List<GalleryModel> galleries;
+
+  ProductModel({
+    this.id,
+    this.name,
+    this.price,
+    this.description,
+    this.tags,
+    this.category,
+    this.createdAt,
+    this.updatedAt,
+    this.galleries,
+  });
+
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    price = double.parse(json['price'].toString());
+    description = json['description'];
+    tags = json['tags'];
+    category = CategoryModel.fromJson(json['category']);
+    galleries = json['galleries']
+        .map((gallery) => GalleryModel.fromJson(gallery))
+        .toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'url': url,
+    };
+  }
+}
