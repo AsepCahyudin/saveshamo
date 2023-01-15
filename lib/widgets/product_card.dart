c50176840cc85279af82416462096f116651d1b2
unknown <asep.cahyudin92@gmail.com>
2023-01-15 14:47:42 +0700

    update product card

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -26,19 +26,28 @@ class ProductCard extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
-            Image.network(
-              product.galleries[0].url,
-              width: 215,
-              height: 150,
-              fit: BoxFit.cover,
-            ),
+            // Image.asset(
+            //   'assets/Image_Shoes.png',
+            //   width: 215,
+            //   height: 150,
+            //   fit: BoxFit.cover,
+            // ),
+            product.galleries[0].url != null
+                ? Image.network(
+                    product.galleries[0].url,
+                    width: 64,
+                  )
+                : Image.asset(
+                    'assets/Image_Shoes.png',
+                    width: 64,
+                  ),
             Container(
               margin: EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Hiking',
+                    product.category.name,
                     style: secondaryTextStyle.copyWith(fontSize: 12),
                   ),
                   SizedBox(
