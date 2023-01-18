7111edea06199a34925ac8334817091a3485d993
unknown <asep.cahyudin92@gmail.com>
2023-01-18 11:49:10 +0700

    consume api in product tile

--- lib/widgets/product_tile.dart
+++ lib/widgets/product_tile.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:saveshamo/models/product_model.dart';
 import 'package:saveshamo/theme.dart';
 
 class ProductTile extends StatelessWidget {
+  final ProductModel product;
+  ProductTile(this.product);
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -18,8 +22,8 @@ class ProductTile extends StatelessWidget {
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                'assets/Image_Shoes.png',
+              child: Image.network(
+                product.galleries[0].url,
                 width: 120,
                 height: 120,
                 fit: BoxFit.cover,
@@ -33,24 +37,25 @@ class ProductTile extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Football',
+                      product.category.name,
                       style: secondaryTextStyle.copyWith(fontSize: 12),
                     ),
                     SizedBox(
                       height: 6,
                     ),
                     Text(
-                      'Predator 20.3 Firm Ground',
+                      product.name,
                       style: primaryTextStyle.copyWith(
                         fontSize: 16,
                         fontWeight: semiBold,
                       ),
+                      maxLines: 1,
                     ),
                     SizedBox(
                       height: 6,
                     ),
                     Text(
-                      '\$75.45',
+                      '\$${product.price}',
                       style: priceTextStyle.copyWith(fontWeight: medium),
                     )
                   ]),
