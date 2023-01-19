b13e5c6942080d1953ef5fe6e3f2746e7bc57b25
unknown <asep.cahyudin92@gmail.com>
2023-01-19 10:45:07 +0700

    new navigation product page

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:saveshamo/models/product_model.dart';
 import 'package:saveshamo/theme.dart';
 
+import '../pages/product_page.dart';
+
 class ProductCard extends StatelessWidget {
   final ProductModel product;
   ProductCard(this.product);
@@ -10,7 +12,12 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.pushNamed(context, '/product');
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProductPage(product),
+          ),
+        );
       },
       child: Container(
         width: 215,
